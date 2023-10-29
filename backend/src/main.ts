44e0f9f10163caa75a844b380489970063b5d604
Mitsu325 <patricia.mashiba@gmail.com>
2023-10-28 22:34:15 -0300

    feat: create user CRUD

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
       'O Swagger (aka OpenApi) é uma biblioteca muito conhecida no universo backend, estando disponível para diversas linguagens e frameworks. Ela gera um site interno no seu backend que descreve, com muitos detalhes, cada endpoint e estrutura de entidades presentes na sua aplicação.',
     )
     .setVersion('1.0')
-    .addTag('users')
+    .addTag('user')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
---
+++ backend/src/user/entities/user.entity.ts
@@ -0,0 +1,28 @@
+import { BaseEntity, Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class User extends BaseEntity {
+	@PrimaryGeneratedColumn('uuid')
+	id: string;
+
+	@Column({ type: 'varchar', length: 200 })
+	name: string;
+
+	@Column({ type: 'varchar', length: 200 })
+	username: string;
+
+	@Column({ type: 'varchar' })
+	password: string;
+
+	@Column({ type: 'varchar', nullable: true })
+	avatar: string;
+
+	@Column({ name: 'two_factor_auth', type: 'boolean', default: false })
+	twoFactorAuth: boolean;
+
+	@CreateDateColumn({ name: 'created_at', type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+	createdAt: Date;
+
+	@UpdateDateColumn({ name: 'updated_at', type: "timestamp", default: () => 'CURRENT_TIMESTAMP', onUpdate: 'CURRENT_TIMESTAMP' })
+	updatedAt: Date;
+}
