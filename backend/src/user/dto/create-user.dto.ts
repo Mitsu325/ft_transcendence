2607aab546a8f1e2b36eb9d8f388c6772262eb9d
nandajfa <nanda.jfa@gmail.com>
2023-10-30 12:09:24 -0300

    fix: change validations

--- backend/src/user/dto/create-user.dto.ts
+++ backend/src/user/dto/create-user.dto.ts
@@ -1,17 +1,22 @@
-import { IsString, IsEmail, IsNotEmpty, Matches } from 'class-validator';
+import { IsString, IsEmail, IsNotEmpty, Matches, MaxLength, MinLength, IsDefined } from 'class-validator';
 
 export class CreateUserDto {
     @IsString()
     @IsNotEmpty()
+    @MinLength(3)
+    @MaxLength(200)
     name: string;
 
     @IsString()
     @IsNotEmpty()
+    @MinLength(3)
+    @MaxLength(200)
     username: string;
 
     @IsNotEmpty()
-    @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, {
-        message: 'The password must contain at least one uppercase letter, one lowercase letter, one number and one special character, with a minimum of 8 characters.',
+    @MinLength(8, { message: 'The password must be at least 8 characters.', })
+    @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])/, {
+        message: 'The password must contain at least one uppercase letter, one lowercase letter, one number and one special character.',
     })
     password: string;
 }
