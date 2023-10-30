ff57419b5212d8dfcd57c5b25aa76a463b74ca37
nandajfa <nanda.jfa@gmail.com>
2023-10-30 12:20:57 -0300

    fix: change validationPipe

--- backend/src/user/pipes/verifyUsername.validation.pipe.ts
+++ backend/src/user/pipes/verifyUsername.validation.pipe.ts
@@ -1,6 +1,7 @@
 import { PipeTransform, Injectable, ArgumentMetadata, BadRequestException } from '@nestjs/common';
 import { UserService } from '../user.service';
 import { CreateUserDto } from '../dto/create-user.dto'
+import { isEmail } from 'class-validator';
 
 @Injectable()
 export class UsernameExistsValidationPipe 
@@ -9,12 +10,19 @@ export class UsernameExistsValidationPipe
   constructor(private readonly userService: UserService) {}
 
   async transform(value: CreateUserDto , metadata: ArgumentMetadata) {
+    const { username } = value;
 
+    if (username.includes('@')) {
+    
+      if (!isEmail(username)){
+        throw new BadRequestException('Invalid email.');
+      }
+    } 
+    
     const userExists = await this.userService.findUser(value.username);
     if (userExists) {
       throw new BadRequestException('Username is already in use.');
     }
-
     return value;
   }
 }
