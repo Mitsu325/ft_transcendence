de3dbaf78acde5ac7ad0a5ff7688d3aee39005d5
Mitsu325 <patricia.mashiba@gmail.com>
2023-10-31 23:39:12 -0300

    chore: add editorconfig, eslint and prettier in front

--- frontend/src/routes/AppRouter.tsx
+++ frontend/src/routes/AppRouter.tsx
@@ -1,19 +1,19 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "../components/HomePage";
-import LoginPage from "../components/LoginPage";
-import SignUpPage from "../components/SignUpPage";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from '../components/HomePage';
+import LoginPage from '../components/LoginPage';
+import SignUpPage from '../components/SignUpPage';
 
 const AppRouter = () => {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" Component={HomePage} />
-                <Route path="/login" Component={LoginPage} />
-                <Route path="/signup" Component={SignUpPage} />
-            </Routes>
-        </Router>
-    );
-}
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" Component={HomePage} />
+        <Route path="/login" Component={LoginPage} />
+        <Route path="/signup" Component={SignUpPage} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default AppRouter;
