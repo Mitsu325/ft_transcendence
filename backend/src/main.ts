05535a2bf2a92f65e7c50073e35a676e5812993d
Mitsu325 <patricia.mashiba@gmail.com>
2023-11-01 17:43:21 -0300

    feat: enable cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -11,12 +11,19 @@ async function bootstrap() {
             'O Swagger (aka OpenApi) é uma biblioteca muito conhecida no universo backend, estando disponível para diversas linguagens e frameworks. Ela gera um site interno no seu backend que descreve, com muitos detalhes, cada endpoint e estrutura de entidades presentes na sua aplicação.',
         )
         .setVersion('1.0')
+        .addTag('auth')
         .addTag('user')
         .build();
 
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api', app, document);
 
-    await app.listen(3000);
+    app.enableCors({
+        allowedHeaders: '*',
+        origin: 'http://localhost:3000',
+        credentials: true,
+    });
+
+    await app.listen(3003);
 }
 bootstrap();
