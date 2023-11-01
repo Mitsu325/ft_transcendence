a2ce0618bcdebb931c14bf78bfacddf73ac5a8ef
Mitsu325 <patricia.mashiba@gmail.com>
2023-11-01 17:44:19 -0300

    feat: add login with intra 42

--- frontend/src/components/LoginPage.tsx
+++ frontend/src/components/LoginPage.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link } from 'react-router-dom'; // Importe Link do React Router
+import React, { useEffect } from 'react';
+import { createSearchParams, useSearchParams } from 'react-router-dom'; // Importe Link do React Router
 import {
   Breadcrumb,
   Layout,
@@ -9,15 +9,57 @@ import {
   Row,
   Typography,
   Space,
+  Button,
 } from 'antd';
-
+import axios from 'axios';
 const { Header, Content, Footer } = Layout;
 const { Text } = Typography;
 
-const HomePage: React.FC = () => {
+const CLIENT_ID =
+  'u-s4t2ud-69c6515e1aadb326fe9b48fc0b673b271390fbb38afb06138005fbf548933f38';
+
+const LoginPage: React.FC = () => {
+  const [searchParams] = useSearchParams();
   const {
     token: { colorBgContainer },
   } = theme.useToken();
+
+  useEffect(() => {
+    const codeParam = searchParams.get('code');
+    if (codeParam) {
+      const params = {
+        code: codeParam,
+      };
+      axios
+        .post(
+          'http://localhost:3003/auth/42?' +
+            createSearchParams(params).toString(),
+        )
+        .then(response => {
+          console.log('response');
+          console.log(response);
+          // TODO: direcionar para a página do jogo
+        })
+        .catch(error => {
+          console.log('error');
+          console.error(error);
+          // TODO: mostrar uma mensagem de erro
+        });
+    }
+  }, [searchParams]);
+
+  function loginWith42() {
+    const params = {
+      client_id: CLIENT_ID,
+      redirect_uri: 'http://localhost:3000/login',
+      response_type: 'code',
+    };
+
+    window.location.assign(
+      'https://api.intra.42.fr/oauth/authorize?' +
+        createSearchParams(params).toString(),
+    );
+  }
 
   return (
     <Layout>
@@ -34,13 +76,10 @@ const HomePage: React.FC = () => {
         <div className="demo-logo" />
       </Header>
       <Content className="site-layout" style={{ padding: '0 50px' }}>
-        <Breadcrumb style={{ margin: '16px 0' }}>
-          <Breadcrumb.Item>
-            <Link to="/" style={{ color: 'black', textDecoration: 'none' }}>
-              <h3>Home</h3>
-            </Link>
-          </Breadcrumb.Item>
-        </Breadcrumb>
+        <Breadcrumb
+          style={{ margin: '16px 0' }}
+          items={[{ title: 'Home', href: '/' }]}
+        />
         <div style={{ padding: 24, background: colorBgContainer }}>
           <div style={{ padding: 50, background: colorBgContainer }}>
             <Divider></Divider>
@@ -54,7 +93,15 @@ const HomePage: React.FC = () => {
               </Text>
             </Space>
             <Row>
-              <Col span={8}></Col>
+              <Col span={8}>
+                <Button
+                  type="primary"
+                  style={{ minWidth: 200 }}
+                  onClick={loginWith42}
+                >
+                  Login com Intra 42
+                </Button>
+              </Col>
               <Col
                 span={8}
                 style={{ display: 'flex', justifyContent: 'center' }}
@@ -73,4 +120,4 @@ const HomePage: React.FC = () => {
   );
 };
 
-export default HomePage;
+export default LoginPage;
