05b42907b6a34db3c9134af0fc36a675a0fcf991
Mitsu325 <patricia.mashiba@gmail.com>
2023-11-05 10:57:20 -0300

    fix: merge

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,6 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    const app = await NestFactory.create(AppModule);
 
     const config = new DocumentBuilder()
         .setTitle('Documentação com Swagger - Fábrica de Sinapse')
@@ -18,16 +17,7 @@ async function bootstrap() {
 
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api', app, document);
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api', app, document);
 
-    app.enableCors({
-        allowedHeaders: '*',
-        origin: 'http://localhost:3000',
-        credentials: true,
-    });
-
-    await app.listen(3003);
     app.enableCors({
         allowedHeaders: '*',
         origin: 'http://localhost:3000',
