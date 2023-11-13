01aaba89f34b70d169250b1cfe16f3ed9baf2344
Mitsu325 <patricia.mashiba@gmail.com>
2023-11-13 01:40:18 -0300

    chore: add swagger details

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,10 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Public } from './common/constants';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
     constructor(private readonly appService: AppService) {}
 
+    @ApiOperation({ description: 'Hello World' })
+    @Public()
     @Get()
     getHello(): string {
         return this.appService.getHello();
