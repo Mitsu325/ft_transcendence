1214f3d784f808d240cde015516fabee8529746d
marcocslima@gmail.com <marcocslima@gmail.com>
2023-11-23 05:44:11 -0300

    feat: created list of players

--- backend/src/game/game.controller.ts
+++ backend/src/game/game.controller.ts
@@ -11,23 +11,40 @@ import {
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 
+const players = [];
+
 @ApiTags('pong')
 @Controller('pong')
-@WebSocketGateway({ cors: { origin: 'http://localhost:3000', } })
+@WebSocketGateway({ cors: { origin: 'http://localhost:3000' } })
 export class GamePong implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
   server: Server;
+
+  players: any[] = [];
 
   handleConnection(client: Socket, ...args: any[]) {
     console.log(client.id + ' connected !!!');
   }
 
   handleDisconnect(client: Socket) {
-    console.log(client.id + ' disconnected ...');
+    const index = players.findIndex((p) => p.sid === client.id);
+    if (index !== -1) {
+      players.splice(index, 1);
+    }
+    this.server.emit('players', players);
+    console.log(players);
   }
 
-  @SubscribeMessage('message')
-  handleMessage(@MessageBody() data: string, @ConnectedSocket() client: Socket) {
-    this.server.emit('message', data);
+  @SubscribeMessage('PlayerConnected')
+  handlePlayerConnected(@MessageBody() player: any, @ConnectedSocket() client: Socket) {
+    const existingPlayerIndex = players.findIndex((p) => p.id === player.id);
+
+    if (existingPlayerIndex === -1) {
+      players.push({ id: player.id, sid: client.id, ...player });
+    } else {
+      console.log('Player with the same ID already exists:', player.id);
+    }
+    this.server.emit('players', players);
+    console.log(players);
   }
 }
--- frontend/src/components/Game/index.tsx
+++ frontend/src/components/Game/index.tsx
@@ -1,7 +1,36 @@
 import * as React from 'react';
 import io from 'socket.io-client';
+import { useAuth } from 'hooks/useAuth';
+import { Avatar } from 'antd';
+import './style.css';
+
+interface Player {
+  id: string;
+  name: string;
+  avatar: string;
+}
+
+interface PlayerCardProps {
+  player: Player;
+}
+
+const PlayerCard: React.FC<PlayerCardProps> = ({ player }) => (
+  <div key={player.id} className="player-card">
+    <Avatar src={player.avatar} style={{ backgroundColor: '#f56a00' }}>
+      {player.name.substring(0, 1).toUpperCase()}
+    </Avatar>
+    <div className="player-info">
+      <p className="player-name">{player.name}</p>
+    </div>
+  </div>
+);
 
 export const Game = () => {
+  const user = useAuth();
+  const [players, setPlayers] = React.useState<
+    Array<{ id: string; name: string; avatar: string }>
+  >([]);
+
   const socket = React.useMemo(() => {
     const newSocket = io('http://localhost:3003', {
       reconnectionDelay: 10000,
@@ -10,19 +39,33 @@ export const Game = () => {
   }, []);
 
   React.useEffect(() => {
+    const player = {
+      id: user?.user?.id,
+      name: user?.user?.username,
+      avatar: user?.user?.avatar,
+    };
+
     socket.on('connect', () => {
-      console.log('connected client');
+      socket.emit('PlayerConnected', player);
+    });
+
+    socket.on('players', players => {
+      setPlayers(players);
     });
 
     return () => {
-      // Certifique-se de desconectar o socket quando o componente for desmontado
       socket.disconnect();
     };
-  }, [socket]);
+  }, [socket, user?.user?.id, user?.user?.username, user?.user?.avatar]);
 
   return (
     <div>
-      <h1>*** Game - Teste ***</h1>
+      <h1>*** JOGADORES ***</h1>
+      <div className="players-container">
+        {players.map(player => (
+          <PlayerCard key={player.id} player={player} />
+        ))}
+      </div>
     </div>
   );
 };
