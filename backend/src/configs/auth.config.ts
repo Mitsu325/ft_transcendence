9a154b1a730d7b0b64b7ab6c0ae97a21efd7da04
Mitsu325 <patricia.mashiba@gmail.com>
2023-12-23 20:42:43 -0300

    chore: update 42 api secret

--- backend/src/configs/auth.config.ts
+++ backend/src/configs/auth.config.ts
@@ -2,7 +2,7 @@ export default {
     clientId:
         'u-s4t2ud-69c6515e1aadb326fe9b48fc0b673b271390fbb38afb06138005fbf548933f38',
     clientSecret:
-        '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',
+        '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',
     redirectUri: 'http://localhost:3000',
     tokenUrl: 'https://api.intra.42.fr/oauth/token',
     getResourceOwnerUrl: 'https://api.intra.42.fr/v2/me',
