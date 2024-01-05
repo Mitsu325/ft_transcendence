cf96ff98a1f0545f740019e36d8bf76bf5ff62c5
Mitsu325 <patricia.mashiba@gmail.com>
2024-01-04 21:04:37 -0300

    chore: update client secret

--- backend/src/configs/auth.config.ts
+++ backend/src/configs/auth.config.ts
@@ -2,7 +2,7 @@ export default {
     clientId:
         'u-s4t2ud-69c6515e1aadb326fe9b48fc0b673b271390fbb38afb06138005fbf548933f38',
     clientSecret:
-        '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',
+        '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',
     redirectUri: 'http://localhost:3000',
     tokenUrl: 'https://api.intra.42.fr/oauth/token',
     getResourceOwnerUrl: 'https://api.intra.42.fr/v2/me',
