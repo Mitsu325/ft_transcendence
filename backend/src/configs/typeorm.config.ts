b646281cb8e4990c1ac43efe6be6dfb75a027c42
Mitsu325 <patricia.mashiba@gmail.com>
2024-02-09 22:49:46 -0300

    feat: connect backend with database

--- backend/src/configs/typeorm.config.ts
+++ backend/src/configs/typeorm.config.ts
@@ -4,11 +4,21 @@ dotenv.config();
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
     type: 'postgres',
-    host: process.env.DB_HOST || 'localhost',
+    host: process.env.POSTGRES_HOST,
     port: parseInt(process.env.POSTGRES_PORT),
     username: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
     entities: [__dirname + '/../**/*.entity.{js,ts}'],
-    synchronize: true,
+    autoLoadEntities: true,
 };
+
+export const OrmConfig = {
+    ...typeOrmConfig,
+    migrationsTableName: 'migrations',
+    migrations: ['src/migrations/*.ts'],
+    cli: {
+        migrationsDir: 'src/migrations',
+    },
+};
+export default OrmConfig;
